e83a8b8558479572b5b944ad9f540ec763e8d000
ambucha <ambuchatskiy@gmail.com>
2025-09-19 21:25:30 -0700

    THird Comit

--- lab-11.cpp
+++ lab-11.cpp
@@ -2,6 +2,35 @@
 // IDE used : Visual Studio Code
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-// Brainstorming idea
+// Brainstorming idea:
+/* Could just be an array with like music artists I like, then inside there could be an array for like
+   favorite songs, could be different the ammount of songs I like depending on the artist, then maybe just pure stats like monthly listenenrs
+   and maybe one more stat like... style of music? I guess, that coyuld change depending lets see, full of strings but its fine i guess*/
+
+// Create a struct to hold the artists
+struct Artists {
+    string name;
+    int monthlyListenners;
+    string * favSongs;
+
+    // Create destructor for the favSongs array
+    ~Artists() {
+        if (favSongs){
+            delete[] favSongs;
+        }
+        favSongs = nullptr;
+    }
+};
+
+// inputArtist() collects data for the artist
+// arguments: pointer
+// return: nothing
+void inputArtist(Artists *);
+
+// displayArtist() displays data for the artist
+// arguments: pointer
+// return: nothing
+void displayArtist(Artists *);
