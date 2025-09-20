514696653350ed37efb6dfdffb1de3f59438f97a
ambucha <ambuchatskiy@gmail.com>
2025-09-19 21:37:03 -0700

    Fourht COmmit

--- lab-11.cpp
+++ lab-11.cpp
@@ -26,11 +26,51 @@ struct Artists {
 };
 
 // inputArtist() collects data for the artist
-// arguments: pointer
+// arguments: pointer, array size
 // return: nothing
 void inputArtist(Artists *);
 
 // displayArtist() displays data for the artist
-// arguments: pointer
+// arguments: pointer, array size
 // return: nothing
 void displayArtist(Artists *);
+
+int main(){
+    // Collect the number of artists you would like to input
+    int numOfA;
+    cout << "Enter # of Artists: ";
+    cin >> numOfA;
+
+    // Create dynamic array of artists
+    Artists *list = new Artists[numOfA];
+
+    // Create for loop to collect Artist data
+    for(int i = 0; i < numOfA; i++){
+        inputArtist(&list[i]);
+    }
+
+    // Create for loop to display data
+    for(int i = 0; i < numOfA; i++){
+        displayArtist(&list[i]);
+    }
+}
+
+void inputArtist(Artists * aptr){
+    static int nrA = 1;
+    int numSongs;
+    cout << "Input data for artist #" << nrA << ": " << endl;
+    cout << "Name: ";
+    getline(cin, aptr->name);
+    cout << "Monthly Listenners: ";
+    cin >> aptr->monthlyListenners;
+    cout << "Enter # of songs of theirs you enjoy: ";
+    cin >> numSongs;
+    for(int i = 0; i < numSongs; i++){
+        cout << "Favorite Song #" << i + 1 << ": ";
+        cin.ignore();
+        getline(cin, aptr->favSongs[i]);
+    }
+    cin.ignore();
+    cout << endl;
+    nrA++;
+}
